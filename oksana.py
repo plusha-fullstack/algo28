faf6c9274a60643aaf35becbd397160b5f5da3df
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-07-25 17:56:03 +0300

    Update oksana.py

--- oksana.py
+++ oksana.py
@@ -1,4 +1,10 @@
 def odometr(arr):
-    new_arr = arr[::2]
-    return sum(new_arr)
+    hours = arr[1::2]
+    speed = arr[::2]
+    print(hours)
+    print(speed)
+    summ = speed[0]*hours[0] 
+    for i in range(1,len(hours)):
+        summ+=speed[i]*(hours[i]-hours[i-1])
+    return summ
     
