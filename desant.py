1174bb39c9ca33a536bc95a166b7b026b3afa53e
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-07-28 21:07:06 +0300

    Update desant.py

--- desant.py
+++ desant.py
@@ -6,64 +6,83 @@ def ConquestCampaign(N, M, L, battalion):#n-shirina m-dlina
         y = battalion[k+1]
         battlefield[x-1][y-1] = 1
     
+    days_of_war = 1
+    number_of_elements = N*M
+    war = True
     
+    while war:
+    
+        for i in range(N):
+            for j in range(M):
+                #angles
+                if True:
+                    if (i == 0 and j == 0):
+                        if battlefield[i+1][j] == 1 or battlefield[i][j+1] == 1:
+                            battlefield[i][j]=2
+                    
+                    if (i == N - 1 and j == 0):
+                        if battlefield[i-1][j] == 1 or battlefield[i][j+1] == 1:
+                            battlefield[i][j]=2
+                            
+                    if (i == 0 and j == M - 1):
+                        if battlefield[i+1][j] == 1 or battlefield[i][j-1] == 1:
+                            battlefield[i][j]=2
+                    if (i == N-1 and j == M-1):
+                        if battlefield[i-1][j] == 1 or battlefield[i][j-1] == 1:
+                            battlefield[i][j]=2
+                
+                
+                #sides
+                if True:
+                    if (i != 0 and j == 0 and i!= N-1):
+                        if battlefield[i+1][j] == 1 or battlefield[i-1][j]==1 or battlefield[i][j+1]==1:
+                            battlefield[i][j]=2
+                    
+                    if (i == 0 and j != 0 and j!= M-1):
+                        if battlefield[i][j+1] == 1 or battlefield[i][j-1]==1 or battlefield[i+1][j]==1:
+                            battlefield[i][j]=2
+                            
+                    if (i != 0 and j == M - 1 and i!= N-1):
+                        if battlefield[i+1][j] == 1 or battlefield[i-1][j]==1 or battlefield[i][j-1]==1:
+                            battlefield[i][j]=2
+                            
+                    if (i == N-1 and j !=0 and j!= M - 1):
+                        if battlefield[i][j-1] == 1 or battlefield[i][j+1] or battlefield[i-1][j]:
+                            battlefield[i][j]=2
+                        
+                
+                if i>0 and j>0 and i<N-1 and j<M-1:
+                    if battlefield[i+1][j] == 1 or battlefield[i-1][j]==1 or battlefield[i][j+1]==1 or battlefield[i][j-1]==1:
+                        battlefield[i][j] = 2
+                        
+                
+        for i in range(N):
+            for j in range(M):
+                if battlefield[i][j] == 2:
+                    battlefield[i][j] = 1
+                    
+        days_of_war+=1
+        summ=0
+        for row in battlefield:
+            for element in row:
+                summ+=battlefield[i][j]
+                print(summ)
+                    
+        for row in battlefield:         
+            for elem in row:
+                print(elem, end = ' ')
+            print()
         
+        if summ == number_of_elements:
+            war = False
     
     
-    for i in range(N):
-        for j in range(M):
-            #angles
-            if True:
-                if (i == 0 and j == 0):
-                    if battlefield[i+1][j] == 1 or battlefield[i][j+1] == 1:
-                        battlefield[i][j]=2
-                
-                if (i == N - 1 and j == 0):
-                    if battlefield[i-1][j] == 1 or battlefield[i][j+1] == 1:
-                        battlefield[i][j]=2
-                        
-                if (i == 0 and j == M - 1):
-                    if battlefield[i+1][j] == 1 or battlefield[i][j-1] == 1:
-                        battlefield[i][j]=2
-                if (i == N-1 and j == M-1):
-                    if battlefield[i-1][j] == 1 or battlefield[i][j-1] == 1:
-                        battlefield[i][j]=2
-            
-            
-            #sides
-            if True:
-                if (i != 0 and j == 0 and i!= N-1):
-                    if battlefield[i+1][j] == 1 or battlefield[i-1][j]==1 or battlefield[i][j+1]==1:
-                        battlefield[i][j]=2
-                
-                if (i == 0 and j != 0 and j!= M-1):
-                    if battlefield[i][j+1] == 1 or battlefield[i][j-1]==1 or battlefield[i+1][j]==1:
-                        battlefield[i][j]=2
-                        
-                if (i != 0 and j == M - 1 and i!= N-1):
-                    if battlefield[i+1][j] == 1 or battlefield[i-1][j]==1 or battlefield[i][j-1]==1:
-                        battlefield[i][j]=2
-                        
-                if (i == N-1 and j !=0 and j!= M - 1):
-                    if battlefield[i][j-1] == 1 or battlefield[i][j+1] or battlefield[i-1][j]:
-                        battlefield[i][j]=2
-                    
-            
-            if i>0 and j>0 and i<N-1 and j<M-1:
-                if battlefield[i+1][j] == 1 or battlefield[i-1][j]==1 or battlefield[i][j+1]==1 or battlefield[i][j-1]==1:
-                    battlefield[i][j] = 2
-                    
-            
-    for i in range(N):
-        for j in range(M):
-            if battlefield[i][j] == 2:
-                battlefield[i][j] = 1
-    
-    
-    for row in battlefield:         
-        for elem in row:
-            print(elem, end = ' ')
-        print()
+        print(summ)
+        print(days_of_war)
+        for row in battlefield:         
+            for elem in row:
+                print(elem, end = ' ')
+            print()
 
 
 ConquestCampaign(3,4,2,[2,2,3,4])
