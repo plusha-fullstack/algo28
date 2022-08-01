ae4cb7b64797e446bac2b293a4c3e88022777608
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-08-01 16:26:23 +0300

    Update madMax.py

--- madMax.py
+++ madMax.py
@@ -1,14 +1,6 @@
 def MadMax(N, Tele):
-    maxx = Tele[0]
-    place_of_max = -1
-    for i in range(N):
-        if Tele[i]>=maxx:
-            maxx = Tele[i]
-            place_of_max = i
+    Tele = sorted(Tele)
     
-    Tele[N//2], Tele[place_of_max] = Tele[place_of_max], Tele[N//2]
-    
-    Tele[:N//2] = sorted(Tele[:N//2])
+    Tele[N//2], Tele[-1] = Tele[-1], Tele[N//2]
     Tele[N//2+1:] = sorted(Tele[N//2+1:],reverse = True)
-    
     return Tele
