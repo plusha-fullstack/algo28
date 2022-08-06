7d0253cbb228457eb15a9e4d46dd4939644b1913
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-08-06 17:54:11 +0300

    Create unlock.py

---
+++ unlock.py
@@ -0,0 +1,14 @@
+def PatternUnlock(N, hits):
+    secret_key = 0
+    for i in range(1,N+1):
+        if hits[i] - hits[i-1] ==1:
+            secret_key+=1
+        else:
+            secret_key+=1.414213
+    
+
+    secret_key = round(secret_key,5)
+    secret_key = str(secret_key)
+    secret_key = secret_key.replace('0', '')
+    secret_key = secret_key.replace('.', '')
+    return secret_key
