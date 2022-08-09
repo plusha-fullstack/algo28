3f2026a2ff8a997446937aafc7b51fa84df55368
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-08-09 19:33:44 +0300

    Update unlock.py

--- unlock.py
+++ unlock.py
@@ -1,10 +1,10 @@
 def PatternUnlock(N, hits):
     secret_key = 0
     for i in range(1,N+1):
-        if hits[i] - hits[i-1] ==1:
-            secret_key+=1
+        if hits[i] - hits[i-1] == 1:
+            secret_key += 1
         else:
-            secret_key+=1.414213
+            secret_key += 2**0.5
     
 
     secret_key = round(secret_key,5)
