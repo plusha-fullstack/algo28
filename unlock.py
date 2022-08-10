df534042ebac560923bd126f6e16820284089599
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-08-10 15:17:07 +0300

    Update unlock.py

--- unlock.py
+++ unlock.py
@@ -1,12 +1,103 @@
 def PatternUnlock(N, hits):
     secret_key = 0
     for i in range(1,N+1):
-        if hits[i] - hits[i-1] == 0:
-            pass
-        elif hits[i] - hits[i-1] == 1:
-            secret_key += 1
-        else:
-            secret_key += 2**0.5
+        if hits[i] == 6:
+            if hits[i-1] == 5:
+                secret_key+=1
+            if hits[i-1] == 1:
+                secret_key+=1
+            if hits[i-1] == 2:
+                secret_key+=2**0.5
+                
+        if hits[i] == 5:
+            if hits[i-1] == 6:
+                secret_key+=1
+            if hits[i-1] == 2:
+                secret_key+=1
+            if hits[i-1] == 4:
+                secret_key+=1
+            if hits[i-1] == 1:
+                secret_key+=2**0.5
+            if hits[i-1] == 3:
+                secret_key+=2**0.5
+                
+        if hits[i] == 4:
+            if hits[i-1] == 5:
+                secret_key+=1
+            if hits[i-1] == 3:
+                secret_key+=1
+            if hits[i-1] == 2:
+                secret_key+=2**0.5
+                
+        if hits[i] == 1:
+            if hits[i-1] == 6:
+                secret_key+=1
+            if hits[i-1] == 9:
+                secret_key+=1
+            if hits[i-1] == 2:
+                secret_key+=1
+            if hits[i-1] == 5:
+                secret_key+=2**0.5
+            if hits[i-1] == 8:
+                secret_key+=2**0.5
+                
+        if hits[i] == 2:
+            if hits[i-1] == 1:
+                secret_key+=1
+            if hits[i-1] == 5:
+                secret_key+=1
+            if hits[i-1] == 3:
+                secret_key+=1
+            if hits[i-1] == 8:
+                secret_key+=1
+            if hits[i-1] == 6:
+                secret_key+=2**0.5
+            if hits[i-1] == 9:
+                secret_key+=2**0.5
+            if hits[i-1] == 4:
+                secret_key+=2**0.5
+            if hits[i-1] == 7:
+                secret_key+=2**0.5
+        
+        if hits[i] == 3:
+            if hits[i-1] == 4:
+                secret_key+=1
+            if hits[i-1] == 2:
+                secret_key+=1
+            if hits[i-1] == 7:
+                secret_key+=1
+            if hits[i-1] == 5:
+                secret_key+=2**0.5
+            if hits[i-1] == 8:
+                secret_key+=2**0.5
+                
+        if hits[i] == 9:
+            if hits[i-1] == 1:
+                secret_key+=1
+            if hits[i-1] == 8:
+                secret_key+=1
+            if hits[i-1] == 2:
+                secret_key+=2**0.5
+        
+        if hits[i] == 8:
+            if hits[i-1] == 2:
+                secret_key+=1
+            if hits[i-1] == 9:
+                secret_key+=1
+            if hits[i-1] == 7:
+                secret_key+=1
+            if hits[i-1] == 1:
+                secret_key+=2**0.5
+            if hits[i-1] == 3:
+                secret_key+=2**0.5
+                
+        if hits[i] == 7:
+            if hits[i-1] == 3:
+                secret_key+=1
+            if hits[i-1] == 8:
+                secret_key+=1
+            if hits[i-1] == 2:
+                secret_key+=2**0.5
     
 
     secret_key = round(secret_key,5)
@@ -14,4 +105,3 @@ def PatternUnlock(N, hits):
     secret_key = secret_key.replace('0', '')
     secret_key = secret_key.replace('.', '')
     return secret_key
-    
