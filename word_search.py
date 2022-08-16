a2934111f1654f1a14548161e822d854e8177177
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-08-16 13:12:00 +0300

    Update word_search.py

--- word_search.py
+++ word_search.py
@@ -1,9 +1,23 @@
 def WordSearch(length, s, subs):
+    itog_array = []
+    temp_str = ''
     arr = s.split()
     for i in range(len(arr)):
-        if subs in arr[i]:
-            arr[i] = 1
+        if (len(temp_str)<length) and (len(temp_str) + len(arr[i])) < length:
+            temp_str += arr[i]
+            temp_str+=' '
         else:
-            arr[i] = 0
+            itog_array.append(temp_str)
+            temp_str = arr[i]
+            temp_str +=' '
+         
+    if (len(temp_str) > 0):
+        itog_array.append(temp_str)   
+    for i in range(len(itog_array)):
+        if subs in itog_array[i]:
+            itog_array[i] = 1
+        else:
+            itog_array[i] = 0
             
-    return arr
+    return itog_array
+    
