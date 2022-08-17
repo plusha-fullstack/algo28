4b150625bd145fbef8b03f0708025f1fd9fcf721
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-08-17 21:45:28 +0300

    Update rabbitFoot.py

--- rabbitFoot.py
+++ rabbitFoot.py
@@ -1,5 +1,6 @@
 def TheRabbitsFoot(s, encode):
-    s = s.replace(" ", "")
+    if encode:
+        s = s.replace(" ", "")
     length = len(s)
     N = (length**0.5)//1
     M = N+1
@@ -25,24 +26,19 @@ def TheRabbitsFoot(s, encode):
             if flag_out:
                 break
     if encode == False:
-        len_of_str = len(s)
-        k = 0
-        flag_out = False
-        for i in range(N):
-            for j in range(M):
-                matrix[i][j] = s[k]
-                k+=1
-                len_of_str -= 1
-                if len_of_str == 0:
-                    flag_out = True
-                if flag_out:
-                    break
-            if flag_out:
-                break
+        matrix = s.split()
+        for i in range(len(matrix)):
+            matrix[i] = list(matrix[i])
     
-    for i in range(N):
-        for j in range(M):
-            print(matrix[i][j],end = ' ')
+    
+    for row in matrix:            # делаем перебор всех строк матрицы A
+        for elem in row:     # перебираем все элементы в строке row
+            print(elem, end = ' ')
+        print()
+        
+     for row in matrix:            # делаем перебор всех строк матрицы A
+        for elem in row:     # перебираем все элементы в строке row
+            print(elem, end = ' ')
         print()
     #generate string
     res_str = ''
@@ -55,8 +51,8 @@ def TheRabbitsFoot(s, encode):
     if encode == False:
         for i in range(N):
             for j in range(M):
-                if matrix[j][i]!='0':
-                    res_str+=matrix[j][i]
+                if matrix[i][j]!='0':
+                    res_str+=matrix[i][j]
         
     
     return res_str
@@ -67,4 +63,3 @@ def TheRabbitsFoot(s, encode):
     
 #print(TheRabbitsFoot('отдай мою кроличью лапку',True))# омоюу толл дюиа акчп йрьк
 print(TheRabbitsFoot('омоюу толл дюиа акчп йрьк',False))#отдаймоюкроличьюлапку
-    
