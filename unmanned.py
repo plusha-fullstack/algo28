750be8e4c0953a56097b3c9ebf2406a7de12afb0
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-08-23 19:32:36 +0300

    Update unmanned.py

--- unmanned.py
+++ unmanned.py
@@ -1,13 +1,19 @@
 def Unmanned(L, N, track):
     res = 0
     for i in range(len(track)):
+        if track[i][0] >= L:
+            continue
         if i > 0:
             res += track[i][0] - track[i - 1][0]
         else:
             res += track[i][0]
         if (res // track[i][1]) % 2 == 0:
             res += track[i][1] - (res % track[i][1])
-            
+    
+    
+    if res == 0:
+        return L
     res += (L - track[-1][0])
+    
     
     return res
