0774a7b4cc8bafc88628cbaf06a5f54c0321e5e7
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-08-24 21:40:12 +0300

    Update tank_rush.py

--- tank_rush.py
+++ tank_rush.py
@@ -1,20 +1,24 @@
-def looking_sub(str1,str2):
-    if str2 in str1:
-        return True
-    return False
-            
-    
-
 def TankRush(H1, W1, S1, H2, W2, S2):
     str1 = S1.split()
     str2 = S2.split()
-    for j in range(len(str2)):
-        flag = False
-        for i in range(len(str1)):
-            if looking_sub(str1[i],str2[j]):
+    for i in range(len(str1)):
+        str1[i] = list(str1[i])
+    for i in range(len(str2)):
+        str2[i] = list(str2[i])
+    flag = True
+    for i in range(len(str1)):
+        for j in range(len(str1[i])):
+            if str2[0][0] == str1[i][j]:
                 flag = True
-                break
-        if flag == False:
-            return flag
-    return flag
+                for m in range(len(str2)):
+                    for k in range(len(str2[m])):
+                        try:
+                            if str1[i + m][j + k] != str2[m][k]:
+                                flag = False
+                        except IndexError:
+                            flag = False
+                if flag == True:
+                    return True
+    return False
+        
     
