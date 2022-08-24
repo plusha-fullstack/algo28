1d83b6659994a7f4510822a5ef923315bf0a1b50
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-08-24 15:27:12 +0300

    Update tank_rush.py

--- tank_rush.py
+++ tank_rush.py
@@ -1,18 +1,20 @@
 def looking_sub(str1,str2):
-    for i in range(len(str1) - len(str2) +1):
-        print(i)
-        for j in range(len(str2)):
-            if str1[i + j ] != str2[j]:
-                break
-        if j == len(str2) - 1:
-            return True
+    if str2 in str1:
+        return True
     return False
-            
             
     
 
 def TankRush(H1, W1, S1, H2, W2, S2):
     str1 = S1.split()
     str2 = S2.split()
-    
+    for j in range(len(str2)):
+        flag = False
+        for i in range(len(str1)):
+            if looking_sub(str1[i],str2[j]):
+                flag = True
+                break
+        if flag == False:
+            return flag
+    return flag
     
