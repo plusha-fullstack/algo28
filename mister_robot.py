18e280c5de48f9b5316a755491396cb03b991ec1
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-09-03 14:08:09 +0300

    Create mister_robot.py

---
+++ mister_robot.py
@@ -0,0 +1,20 @@
+def can_be_sorted(a,b,c):
+    li_st = [a,b,c]
+    order_list = sorted(li_st)
+    if order_list == [c,a,b] or order_list == [b,c,a]:
+        return True
+    return False
+
+
+def MisterRobot(N, data):
+    flag = True
+    while flag:
+        flag = False
+        for i in range(len(data) - 2):
+            if can_be_sorted(data[i], data[i + 1], data[i + 2]):
+                flag = True
+                data[i:i + 3] = sorted(data[i:i + 3])
+        
+    if data == sorted(data):
+        return True
+    return False
