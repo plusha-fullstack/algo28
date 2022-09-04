e1bcc925c66a70e06c6c001e18714168a31b7208
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-09-04 14:42:49 +0300

    Update olap.py

--- olap.py
+++ olap.py
@@ -1,20 +1,6 @@
-#string [] ShopOLAP(int N, string [] items)
 def ShopOLAP(N, items):
     dictionary = {}
-    for i in range(len(items)):
-        parsed = items[i].split(' ')
-        dictionary[parsed[0]] = int(parsed[1])
-        
-    print(dictionary)
-    
-    
-ShopOLAP(0,['платье1 5','сумка32 2','платье1 1','сумка23 2','сумка128 4'])
-
-
-
-#string [] ShopOLAP(int N, string [] items)
-def ShopOLAP(N, items):
-    dictionary = {}
+    out = []
     for i in range(len(items)):
         parsed = items[i].split(' ')
         if parsed[0] in dictionary:
@@ -22,13 +8,12 @@ def ShopOLAP(N, items):
         else:
             dictionary[parsed[0]] = int(parsed[1])
     
+    sorted_tuple = sorted(dictionary.items(), key=lambda x: x[0])
+    dictionary = dict(sorted_tuple)
     sorted_tuple = sorted(dictionary.items(), key=lambda x: x[1],reverse = True)
     dictionary = dict(sorted_tuple)
-        #for j in range(0, len(dictionary) - i - 1):
-            #if dictionary[i] == dictionary[i + 1]:
-                
-        
-    print(dictionary)
     
+    for k, v in dictionary.items():
+        out.append(str(k) + ' ' + str(dictionary[k]))
     
-ShopOLAP(0,['платье1 5','сумка32 2','платье1 1','сумка23 2','сумка128 4'])
+    return out
