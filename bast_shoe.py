6bc39b1cf8bea442e3ece296a0d993da410ed76e
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-09-06 23:58:33 +0300

    Update bast_shoe.py

--- bast_shoe.py
+++ bast_shoe.py
@@ -12,5 +12,23 @@ def BastShoe(command):
         return current_str
     
     if operation == '2':
+        if not command[2:].isnumeric():
+            return current_str
         n = int(command[2:])
+        if n > len(current_str):
+            current_str = ''
+            list_of_instances.append(current_str)
+            return current_str
+        else:
+            current_str = current_str[:-n]
+            list_of_instances.append(current_str)
+            return current_str
+    
+    if operation == '3':
+        if not command[2:].isnumeric():
+            return current_str
+        digit = int(command[2:])
+        if len(current_str) <= digit:
+            return ''
+        return current_str[digit]
         
