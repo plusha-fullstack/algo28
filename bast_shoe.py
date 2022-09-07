bc46637f792d46c2cd47ed27e8e636c0b1c78055
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-09-07 20:11:39 +0300

    Update bast_shoe.py

--- bast_shoe.py
+++ bast_shoe.py
@@ -1,27 +1,36 @@
 list_of_instances = []
 current_str = ''
+where_i_am = -1
 def BastShoe(command):
     global current_str
     global list_of_instances
+    global where_i_am
     operation = command[0]
     if not operation.isnumeric():
         return current_str
     if operation == '1':
+        if where_i_am < len(list_of_instances) - 1:
+            list_of_instances = list_of_instances[:where_i_am + 1]
         current_str += command[2:]
         list_of_instances.append(current_str)
+        where_i_am += 1
         return current_str
     
     if operation == '2':
         if not command[2:].isnumeric():
             return current_str
+        if where_i_am < len(list_of_instances) - 1:
+            list_of_instances = list_of_instances[:where_i_am + 1]
         n = int(command[2:])
         if n > len(current_str):
             current_str = ''
             list_of_instances.append(current_str)
+            where_i_am += 1
             return current_str
         else:
             current_str = current_str[:-n]
             list_of_instances.append(current_str)
+            where_i_am += 1
             return current_str
     
     if operation == '3':
@@ -32,3 +41,14 @@ def BastShoe(command):
             return ''
         return current_str[digit]
         
+    if operation == '4':
+        if where_i_am > 0:
+            current_str = list_of_instances[where_i_am - 1]
+            where_i_am -= 1
+        return current_str
+    
+    if operation == '5':
+        if where_i_am < len(list_of_instances) - 1:
+            current_str = list_of_instances[where_i_am + 1]
+            where_i_am +=1
+        return current_str
