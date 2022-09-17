3319b0f8627f8c124e572ba8a16ea2f5fca6dfda
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-09-17 12:24:09 +0300

    Update tree_of_life.py

--- tree_of_life.py
+++ tree_of_life.py
@@ -4,6 +4,45 @@ def rost(H,W,tree):
             tree[i][j] += 1
                     
     return tree
+
+def withering(H,W,tree):
+    for i in range(H):
+        for j in range(W):
+            tree[i][j] += 1
+    for i in range(H):
+        for j in range(W):
+            try:
+                if tree[i + 1][j] > 2 and tree[i][j] < 3:
+                    tree[i][j] = 0
+                    continue
+            except IndexError:
+                pass
+            try:
+                if tree[i - 1][j] > 2 and tree[i][j] < 3:
+                    tree[i][j] = 0
+                    continue
+            except IndexError:
+                pass
+            try:
+                if tree[i][j + 1] > 2 and tree[i][j] < 3:
+                    tree[i][j] = 0
+                    continue
+            except IndexError:
+                pass
+            try:
+                if tree[i][j - 1] > 2 and tree[i][j] < 3:
+                    tree[i][j] = 0
+                    continue
+            except IndexError:
+                pass
+            
+    for i in range(H):
+        for j in range(W):
+            if tree[i][j] > 2:
+                tree[i][j] = 0
+    
+    return tree
+    
                 
     
     
@@ -17,26 +56,18 @@ def TreeOfLife(H, W, N, tree):
             else:
                 tree[i][j] = 0
     for i in range(N):
-        if N % 2 == 0:
+        if i % 2 == 0:
             tree = rost(H,W,tree)
+        else:
+            tree = withering(H,W,tree)
         
-        
+    for i in range(H):
+        for j in range(W):
+            if tree[i][j] > 0:
+                tree[i][j] = '+'
+            else:
+                tree[i][j] = '.'
+                
+    for i in range(len(tree)):
+        tree[i] = ''.join(tree[i])
     return tree
-            
-            
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-print(TreeOfLife(3,4,12, [".+..","..+.",".+.."]))
