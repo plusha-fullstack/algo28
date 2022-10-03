92b0edf93a9414da6d6555775a8aab1b1aeeab7b
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-10-04 00:17:39 +0300

    Create walkers.py

---
+++ walkers.py
@@ -0,0 +1,26 @@
+def look_right(village, i):
+    for j in range(i + 1,len(village)):
+        if village[j].isdigit():
+            return j
+def emenies_detection(village,left,right):
+    count = 0
+    for k in range(left + 1, right):
+        if village[k] == '=':
+            count += 1
+    return count == 3        
+def white_walkers(village):
+    left,right = 0,0
+    for i in range(len(village)):
+        if village[i].isdigit():
+            left = i
+            right = look_right(village, i)
+            if int(village[left]) + int(village[right]) == 10:
+                if not emenies_detection(village,left,right):
+                    return False
+            left = right
+    return True
+#print(white_walkers("axxb6===4xaf5===eee5"))
+print(white_walkers("5==ooooooo=5=5"))
+print(white_walkers("axxb6===4xaf5===eee5"))
+print(white_walkers("axxb6===4xaf5===eee5"))
+print(white_walkers("axxb6===4xaf5===eee5"))
